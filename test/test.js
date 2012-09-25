64c5bd9ff7a1646adab7b86cac1e0ee793b2bc90
ehynds <ehynds@brightcove.com>
2012-09-25 13:51:00 -0400

    tweak regex so it'll accept logging statements with inner parens

--- test/test.js
+++ test/test.js
@@ -16,74 +16,84 @@ var tests = [
     ""
   ],
   [
-      'console.log(foo)',
-      { replaceWith: "0;" },
-      "0;"
+    'console.log(foo)',
+    { replaceWith: "0;" },
+    "0;"
   ],
   [
-      'console.warn("foo")',
-      { replaceWith: "" },
-      ""
+    'console.warn("foo")',
+    { replaceWith: "" },
+    ""
   ],
   [
-      ' console.group("foo")',
-      { replaceWith: "" },
-      " "
+    ' console.group("foo")',
+    { replaceWith: "" },
+    " "
   ],
   [
-      'console.groupEnd(\'foo\')',
-      { replaceWith: "" },
-      ""
+    'console.groupEnd(\'foo\')',
+    { replaceWith: "" },
+    ""
   ],
   [
-      'console.error()',
-      { replaceWith: "" },
-      ""
+    'console.error()',
+    { replaceWith: "" },
+    ""
   ],
   [
-      'console.log(arg1, arg2, "foo", arg4)',
-      { replaceWith: "" },
-      ""
+    'console.log(arg1, arg2, "foo", arg4)',
+    { replaceWith: "" },
+    ""
   ],
   [
-      'console.warn(foo)',
-      { replaceWith: "" },
-      ""
+    'console.warn(foo)',
+    { replaceWith: "" },
+    ""
   ],
   [
-      'pre console.warn(foo); post',
-      { replaceWith: "" },
-      "pre  post"
+    'pre console.warn(foo); post',
+    { replaceWith: "" },
+    "pre  post"
   ],
   [
-      'pre console.warn(foo) post',
-      { replaceWith: "" },
-      "pre  post"
+    'pre console.warn(foo) post',
+    { replaceWith: "" },
+    "pre  post"
   ],
   [
-      'pre console.log(foo + bar) post',
-      { replaceWith: "" },
-      "pre  post"
+    'pre console.log(foo + bar) post',
+    { replaceWith: "" },
+    "pre  post"
   ],
   [
-      'console.dir("Testing " + foo, bar);foo;',
-      { replaceWith: "bar;" },
-      "bar;foo;"
+    'console.dir("Testing " + foo, bar);foo;',
+    { replaceWith: "bar;" },
+    "bar;foo;"
   ],
   [
-      'console && console.log("hi")',
-      { replaceWith: "0;" },
-      "console && 0;"
+    'console && console.log("hi")',
+    { replaceWith: "0;" },
+    "console && 0;"
   ],
   [
-      'console.log(foo); bar; console.warn("bar")',
-      {},
-      " bar; "
+    'console.log(foo); bar; console.warn("bar")',
+    {},
+    " bar; "
   ],
   [
-      'console.dir({ complex: "objects" }, [ "array" ])',
-      {},
-      ""
+    'console.dir({ complex: "objects" }, [ "array" ])',
+    {},
+    ""
+  ],
+  [
+    'console.log("foo (inner parens)")',
+    {},
+    ""
+  ],
+  [
+    'console.log("foo (inner parens)");foo;console.warn("(lol)")',
+    {},
+    "foo;"
   ]
 ];
 
