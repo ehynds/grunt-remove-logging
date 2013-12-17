a6a4212f3a86c48700156e78689be1387823e9da
ehynds <ehynds@brightcove.com>
2013-12-17 10:12:48 -0500

    DRY up pull request #13

--- tasks/grunt-remove-logging.js
+++ tasks/grunt-remove-logging.js
@@ -14,25 +14,28 @@ module.exports = function(grunt) {
   grunt.registerMultiTask("removelogging", "Remove console logging", function() {
     var opts = this.options();
 
+    var process = function(srcFile) {
+      var result = task(grunt.file.read(srcFile), opts);
+      grunt.log.writeln("Removed " + result.count + " logging statements from " + srcFile);
+      return result;
+    };
+
     this.files.forEach(function(f) {
-      if (typeof f.dest === "undefined") {
-        f.src.forEach(function (srcFile) {
-          var result = task(grunt.file.read(srcFile), opts);
-          grunt.log.writeln("Removed " + result.count + " logging statements from " + srcFile + ". (File Overwritten)");
+      if(typeof f.dest === "undefined") {
+        f.src.forEach(function(srcFile) {
+          var result = process(srcFile);
           grunt.file.write(srcFile, result.src);
         });
       } else {
         var ret = f.src.map(function(srcFile) {
-          if (grunt.file.isFile(srcFile)){
-            var result = task(grunt.file.read(srcFile), opts);
-            grunt.log.writeln("Removed " + result.count + " logging statements from " + srcFile);
-            return result.src;
-          }else{
+          if(grunt.file.isFile(srcFile)){
+            return process(srcFile).src;
+          } else {
             grunt.log.error("File not found " + srcFile);
           }
         }).join("");
 
-        if(ret){
+        if(ret) {
           grunt.file.write(f.dest, ret);
         }
       }
