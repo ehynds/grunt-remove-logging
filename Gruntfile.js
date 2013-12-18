ecd8817f9f8216915f8462ddb2cd253d9a5be4d6
Nathan Hinish <nathan@tri.ad>
2013-12-18 14:28:04 -0800

    Getting watch working and including .jshintrc.
    It looks like test.js changed a lot but I just wrapped the code in a function and indented.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,26 @@
 module.exports = function(grunt) {
 
+  var config = {
+    files: [ "Gruntfile.js", "tasks/**/*.js", "test/**/*.js" ]
+  };
+
   // Project configuration.
   grunt.initConfig({
     nodeunit: {
       all: ["test/**/*.js"]
     },
     watch: {
-      files: "<config:lint.files>",
+      files: config.files,
       tasks: "default"
     },
     jshint: {
-      all: ["grunt.js", "tasks/**/*.js", "test/**/*.js"],
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true,
-        globals: {}
-      }
+      all: config.files
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-nodeunit");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
   grunt.registerTask("default", ["jshint", "nodeunit"]);
 };
